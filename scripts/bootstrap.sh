31a704fc708e641f6cb944c722eb905c8d6177d1
酒井 貴史 <takashi_sakai@dwango.co.jp>
2013-04-22 21:16:05 +0900

    bootstrap処理を追加

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,24 @@
+DOTFILES_DIR="dotfiles"
+DOTFILES_REPOS="git@github.com:kabostime/dotfiles.git"
+
+[[ -e ~/$DOTFILES_DIR ]] || git clone $DOTFILES_REPOS ~/$DOTFILES_DIR
+pushd ~/$DOTFILES_DIR
+ 
+git submodule init
+git submodule update
+
+for i in `ls -a`
+do
+  [ $i = "." ] && continue
+  [ $i = ".." ] && continue
+  [ $i = ".git" ] && continue
+  [ $i = "README.md" ] && continue
+  ln -s -f ~/$DOTFILES_DIR/$i ~/
+done
+
+ln -s -f ~/$DOTFILES_DIR/.zshrc ~/.oh-my-zsh/custom/my.zsh
+ln -s -f ~/$DOTFILES_DIR/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+vim -c ':BundleInstall!' -c ':q!' -c ':q!'
+ 
+popd

c63f9fe293461094261948fba92f03afe088cfc2
酒井 貴史 <takashi_sakai@dwango.co.jp>
2013-05-16 15:15:48 +0900

    add zsh env file

---
+++ .zsh/env.zsh
@@ -0,0 +1,11 @@
+export REPOS=http://svn.o-in.dwango.co.jp
+
+export ANDROID_HOME="/Users/takashi_sakai/Library/android-sdk"
+export ANDROID_SDK_HOME="/Users/takashi_sakai/Library/android-sdk"
+export ANDROID_NDK_HOME=/Users/takashi_sakai/Develop/android-ndk
+export NDK_ROOT=${ANDROID_NDK_HOME}
+
+export PATH=/usr/local/bin:$PATH:$HOME/Develop/play2.0:$HOME/bin
+export PATH=$PATH:${ANDROID_SDK_HOME}/tools:${ANDROID_SDK_HOME}/platform-tools:${ANDROID_NDK_HOME}
+
+
